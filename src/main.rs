84e340b1181e1078e24dc037d9a3d5d53cae0d3b
Matthew Z Haralovich <zon@haralovich.org>
2024-01-06 19:17:44 -0500

    First arg can set content path

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,21 @@ mod error;
 
 use error::Error;
 
-use std::path::{Path, PathBuf};
+use std::{path::{Path, PathBuf}, env};
 
-use rocket::{tokio::fs::{self}, fs::FileServer};
+use rocket::{tokio::fs::{self}, fs::FileServer, State};
 use rocket_dyn_templates::Template;
 use serde::{Serialize, Deserialize};
 
 #[macro_use] extern crate rocket;
 
+const INDEX_FILE: &str = "index.md";
 const HEADER_SEPARATOR: &str = "^^^";
 const DEFAULT_STYLE: &str = "layout";
+
+struct Setup {
+    content: PathBuf
+}
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
 struct Header {
@@ -27,14 +32,14 @@ struct Response {
 }
 
 #[get("/")]
-async fn index() -> Result<Template, Error> {
-    content(Path::new("index.md").to_path_buf()).await
+async fn index(setup: &State<Setup>) -> Result<Template, Error> {
+    content(Path::new(INDEX_FILE).to_path_buf(), setup).await
 }
 
 #[get("/<file..>", rank = 20)]
-async fn content(file: PathBuf) -> Result<Template, Error> {
+async fn content(file: PathBuf, setup: &State<Setup>) -> Result<Template, Error> {
     let title = file.to_str().expect("invalid file").to_string();
-    let path = Path::new("content/").join(file);
+    let path = setup.content.join(file);
     let ext = path
         .extension().expect("invalid path")
         .to_str().expect("invalid path str");
@@ -72,8 +77,19 @@ async fn content(file: PathBuf) -> Result<Template, Error> {
 
 #[launch]
 fn rocket() -> _ {
+    let args: Vec<String> = env::args().collect();
+    let content_path = if args.len() > 1 {
+        Path::new(&args[1])
+    } else {
+        Path::new("./")
+    }.to_path_buf();
+    let setup = Setup {
+        content: content_path.clone()
+    };
+
     rocket::build()
-        .mount("/static", FileServer::from("content/"))
+        .manage(setup)
+        .mount("/static", FileServer::from(content_path))
         .mount("/", routes![index, content])
         .attach(Template::fairing())
 }
